c9e7f32cb8306478be0ac38678eacf963661c695
ghxhub <3513439060@qq.com>
2025-11-11 16:07:44 +0800

    add a change

--- project/app/config.py
+++ project/app/config.py
@@ -3,14 +3,20 @@
 
 import logging
 from functools import lru_cache
-from pydantic_settings import BaseSettings
+# from pydantic_settings import BaseSettings
+from pydantic import BaseSettings
+import os
 
 log = logging.getLogger("uvicorn")
 
 
+# class Settings(BaseSettings):
+#     environment: str = "dev"
+#     testing: bool = bool(0)
+
 class Settings(BaseSettings):
-    environment: str = "dev"
-    testing: bool = bool(0)
+    environment: str = os.getenv('ENVIRONMENT', 'dev')
+    testing: bool = os.getenv('TESTING', '0')
 
 @lru_cache
 def get_settings() -> BaseSettings:
